bae31d6bc6e6ed5883aff2ca5e3ea39635717f17
Giithub-Exploits <your-email@example.com>
2026-06-07 23:19:51 +0530

    setup client with vite and react router

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>zlight-auth</div>
+    </BrowserRouter>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch((err) => console.log('DB error:', err));
+
 app.get('/', (req, res) => {
   res.json({ message: 'zlight-auth server is running!' });
 });
